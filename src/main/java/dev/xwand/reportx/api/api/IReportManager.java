a0bedb61de9b3d850ec23f52b482eae074c52c26
xWand <64597475+xWand@users.noreply.github.com>
2022-07-11 17:32:27 +0100

    Initial

---
+++ src/main/java/dev/xwand/reportx/api/api/IReportManager.java
@@ -0,0 +1,9 @@
+package dev.xwand.reportx.api.api;
+
+/**
+ * @author xWand
+ */
+public interface IReportManager {
+
+    public void createReport(String reporter, String reported, String reason);
+}
