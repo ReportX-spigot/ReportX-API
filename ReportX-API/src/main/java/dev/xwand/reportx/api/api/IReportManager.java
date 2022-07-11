fdbcc4a93d917dc40d2218aad16015de168823f4
xWand <will.rees9132@gmail.com>
2022-07-11 17:31:55 +0100

    a

--- ReportX-API/src/main/java/dev/xwand/reportx/api/api/IReportManager.java
+++
@@ -1,9 +0,0 @@
-package dev.xwand.reportx.api.api;
-
-/**
- * @author xWand
- */
-public interface IReportManager {
-
-    public void createReport(String reporter, String reported, String reason);
-}
